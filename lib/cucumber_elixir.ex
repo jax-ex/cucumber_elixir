9786dc1ec701af23358b30f17bf2b72972cfeb35
Micah Cooper <micah+github@mrmicahcooper.com>
2019-09-12 20:05:53 -0400

    wip

--- lib/cucumber_elixir.ex
+++ lib/cucumber_elixir.ex
@@ -1,18 +1,33 @@
 defmodule CucumberElixir do
-  @moduledoc """
-  Documentation for CucumberElixir.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CucumberElixir.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def tokenize(file) do
+    file = File.read!(file)
+    tokenize_string({[], %{}, nil}, file)
   end
+
+  def tokenize_string({tokens, token, state}, "Feature:" <> tail) do
+    tokenize_string({tokens, %{keyword: :feature}, :feature_name}, tail)
+  end
+
+  def tokenize_string({acc, token, :feature_name}, <<head::binary-1, tail::binary>>) do
+    name = Map.get(token, :name, "") <> head
+    token = Map.put(token, :name, name)
+    tokenize_string({acc, token, :feature_name}, tail)
+  end
+
+  def tokenize_string({acc, token, :feature_description}, <<head::binary-1, tail::binary>>) do
+    description = Map.get(token, :description, "") <> head
+    token = Map.put(token, :description, description)
+    tokenize_string({acc, token, :feature_description}, tail)
+  end
+
+  def tokenize_string({acc, token}, "\n" <> tail, :feature_name) do
+    token =
+      token
+      |> Map.put(:name, name <> "\n")
+      |> Map.put(description: "")
+
+    tokenize_string({acc, token, :feature_description}, tail)
+  end
+
+  def tokenize_string(acc, _string), do: acc
 end
--- test/cucumber_elixir_test.exs
+++ test/cucumber_elixir_test.exs
@@ -2,7 +2,18 @@ defmodule CucumberElixirTest do
   use ExUnit.Case
   doctest CucumberElixir
 
-  test "greets the world" do
-    assert CucumberElixir.hello() == :world
+  import ExUnit.CaptureIO
+
+  test "tokenize a .feature file" do
+    file = "./test/fixtures/test.feature"
+
+    assert CucumberElixir.tokenize(file) == [
+             %{
+               keyword: :feature,
+               name: " Multiple site support",
+               description:
+                 "  Only blog owners can post to a blog, except administrators,\n  who can post to all blogs."
+             }
+           ]
   end
 end
